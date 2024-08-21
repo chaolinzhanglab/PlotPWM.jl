a3f7077bf7db771b25ce69909ad9819506f7bc5b
Documenter.jl <documenter@juliadocs.github.io>
2024-08-21 16:56:55 +0000

    build based on 6a532e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlotPWM","category":"page"},{"location":"#PlotPWM","page":"Home","title":"PlotPWM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlotPWM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlotPWM]","category":"page"}]
+}
